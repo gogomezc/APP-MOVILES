664d7909dd7d17e756f7499d8fb0ab9a50232440
Ayastian <se.ayalas@duocuc.cl>
2024-09-08 15:11:02 -0300

    Datos de input de homepage verificable en dashboard, sin validaciones, modificado dashboard con idea principal (Ingresar asistencia), menú lateral añadido en dashboard para navegar entre vista de contacto (falta crear), vista de nosotros (falta crear) y cerrar sesión

--- Core/src/app/dashboard/dashboard.page.ts
+++ Core/src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
+  usuario: string = "";
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.usuario = params['usuario'] ? params['usuario'] : 'Invitado';
+    });
   }
 
 }
--- Core/src/app/home/home.page.ts
+++ Core/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  usuario: string="";
+  password: string="";
+  
 
-  constructor() {}
+  constructor(private navCtrl: NavController) {}
 
+  goToDashboard(){
+    const nombreUsuario = this.usuario ? this.usuario: 'Invitado';
+    this.navCtrl.navigateForward(['/dashboard', { usuario: this.usuario }]);
+  }
 }
