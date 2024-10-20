b9ce41293a20cfe9e749bdf169ae86dbaf21ed5d
Ayastian <se.ayalas@duocuc.cl>
2024-10-20 18:40:36 -0300

    Add capacitor-file-picker dependency for QR Scanner and Generator

--- Core/src/app/qrscanner/qrscanner.page.ts
+++ Core/src/app/qrscanner/qrscanner.page.ts
@@ -1,4 +1,3 @@
-import { Barcode } from './../../../node_modules/@capacitor-mlkit/barcode-scanning/dist/esm/definitions.d';
 import { Component, OnInit } from '@angular/core';
 import html2canvas from 'html2canvas';
 import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
@@ -6,12 +5,15 @@ import { Share } from '@capacitor/share';
 import { LoadingController, ModalController, Platform } from '@ionic/angular';
 import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
 import { LensFacing,BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { FilePicker } from '@capawesome/capacitor-file-picker';
+
 @Component({
   selector: 'app-qrscanner',
   templateUrl: './qrscanner.page.html',
   styleUrls: ['./qrscanner.page.scss'],
 })
 export class QrscannerPage implements OnInit {
+
 segment = 'generate';
 qrText = 'Test'
 
@@ -23,7 +25,7 @@ scanResult='';
   ) { }
 
   ngOnInit():void {
-
+// ========================Permisos Cámara si es celular  ========================
     if(this.platform.is('capacitor')){
       BarcodeScanner.isSupported().then;
       BarcodeScanner.checkPermissions().then;
@@ -34,6 +36,20 @@ scanResult='';
     
   }
 
+
+  // ========================Leer QR Desde IMG y guardar resultado en scanResult  ========================
+async readBarcodeFromImage(){
+    const {files} = await FilePicker.pickImages({});
+    const path=files[0]?.path;
+    if(!path) return;
+    const {barcodes} = await BarcodeScanner.readBarcodesFromImage({
+      path,
+      formats:[]
+    })
+    this.scanResult= barcodes[0]?.displayValue;
+}
+  
+// ======================== Scan QR por cámara y guardar resultado en scanResult  ========================
 async startScan() {
   const modal = await this.modalController.create({
   component: BarcodeScanningModalComponent,
